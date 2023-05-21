b1562b163ea74ece44bc1d4d8ee146c8140a01bf
svalencia014 <sv.samuel.valencia014@gmail.com>
2023-05-21 17:46:29 -0400

    update

--- helpers/steelseries.js
+++ helpers/steelseries.js
@@ -1,6 +1,6 @@
 const gamesense = require('gamesense-client');
 
-global client;
+var client;
 
 async function initGamesenseClient() {
   /**
@@ -11,12 +11,34 @@ async function initGamesenseClient() {
 
   /**
    * @type {gamesense.Game}
-   */
+  */
   var game = new gamesense.Game("SsOledServer", "Steelseries OLED Server", gamesense.GameColor.SILVER);
 
   /**
    * @type { gamesense.GameClient}
-   */
-  var client = new gamesense.GameClient(game, endpoint);
+  */
+  client = new gamesense.GameClient(game, endpoint);
+  client.registerGame()
+    .then(bindProgressHandler)
+    .then(startProgressUpdates)
+    .catch(logError);
+}
 
+/**
+ * Binds progress handler
+ * @returns {Promise}
+*/
+
+function bindProgressHandler() {
+  var blue = new gamesense.Color(0,0,255);
+  var red = new gamesense.Color(255,0,0);
+  var gradient = new gamesense.GradientColor(red, blue);
+
+  /**
+   * Setup of event handler for keyboard device
+   * @type {gamesense.GameEventHandler} 
+  */
+
+  //* Continue from here
+  //* https://github.com/DaanWet/gamesense-client/blob/master/examples/progressBar.js#L65
 }
